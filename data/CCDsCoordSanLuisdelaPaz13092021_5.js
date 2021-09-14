b8edca83a9f14f34de1ed17df88071e0b218a118
inaeba <mrochag@inaeba.edu.mx>
2021-09-14 12:12:12 -0500

    primera edicion

---
+++ data/CCDsCoordSanLuisdelaPaz13092021_5.js
@@ -0,0 +1,8 @@
+var json_CCDsCoordSanLuisdelaPaz13092021_5 = {
+"type": "FeatureCollection",
+"name": "CCDsCoordSanLuisdelaPaz13092021_5",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "CLAVE DE O": "I-11-003-02", "NOMBRE DEL": "MAGISTERIAL", "TIPO": "INSTITUCIONAL", "SERVICIOS": "SI", "SITUACI�": "EN OPERACI�N", "CR": "C", "CZ": "6", "NOMBRE DE": "SAN LUIS DE LA PAZ", "MUNICIPIO": "SAN LUIS DE LA PAZ", "COMUNIDAD": "SAN LUIS DE LA PAZ", "DOMICILIO": "FERROCARRIL ENTRE NI�OS HEROES Y PROL GALEANA", "COLONIA": "MAGISTERIAL", "CP": "37900", "GEOREFENCI": "https:\/\/goo.gl\/maps\/z6d7cckyMAFWNp98A", "LIGA COMPL": "https:\/\/www.google.com.mx\/maps\/place\/Plaza+Magisterial+Inaeba\/@21.2906182,-100.5162597,18z\/data=!4m5!3m4!1s0x85d4ce625600da25:0xd0e3db89685558ac!8m2!3d21.2906141!4d-100.5154782", "COORDENADA": "21.290725663413532, -100.51546576619609", "LATITUD": 21.290725663413532, "LONGITUD": -100.51546576619609 }, "geometry": { "type": "Point", "coordinates": [ -100.515465766196101, 21.290725663413536 ] } }
+]
+}
